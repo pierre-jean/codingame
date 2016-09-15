ee4da1c19a988fa39f63157a74a89fa0d84a823f
Pierre-Jean Baraud <pierre-jean.baraud@amundi.com>
2016-09-15 17:28:06 +0100

    Fix broken test

--- src/test/java/fr/baraud/codingame/easy/thedescent/PlayerShould.java
+++ src/test/java/fr/baraud/codingame/easy/thedescent/PlayerShould.java
@@ -12,7 +12,7 @@ public class PlayerShould {
 
     @Test
     public void print_index_1_on_shot_HighestMountain_1_4_2_3_4_0_1(){
-        ByteArrayInputStream input = new ByteArrayInputStream(String.format("1%n4%n2%n3%n4%n0%n1%n").getBytes());
+        ByteArrayInputStream input = new ByteArrayInputStream(String.format("1%n4%n2%n3%n4%n0%n1%n0%n").getBytes());
         ByteArrayOutputStream output = new ByteArrayOutputStream();
         Player player = new Player(output);
         player.shotBiggestMountain(Mountains.fromStream(input));
@@ -21,7 +21,7 @@ public class PlayerShould {
 
     @Test
     public void print_index_2_on_shot_HighestMountain_1_4_10_3_4_0_1(){
-        ByteArrayInputStream input = new ByteArrayInputStream(String.format("1%n4%n10%n3%n4%n0%n1%n").getBytes());
+        ByteArrayInputStream input = new ByteArrayInputStream(String.format("1%n4%n10%n3%n4%n0%n1%n0%n").getBytes());
         ByteArrayOutputStream output = new ByteArrayOutputStream();
         Player player = new Player(output);
         player.shotBiggestMountain(Mountains.fromStream(input));
